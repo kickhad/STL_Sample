2913265de94b0cbb971ae804d58f5a5953a171bc
kickhad <adam@adamkickham.com>
2019-03-13 14:33:19 -0400

    RegExp Make columns

---
+++ regexp.r
@@ -0,0 +1,6 @@
+      Region            Banner    MCH1            Description                  Debit Amt
+
+                                                                                   (CAD)
+
+     ThePlace         TheStore AC093 Some Other Stuff                         97.13
+                                           Total (CAD)                           97.13
